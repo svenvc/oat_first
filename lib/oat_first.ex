9ee4c3d350c58fa92bf3c9737903d3cce22ce7e9
Sven Van Caekenberghe <sven@stfx.eu>
2026-05-04 17:36:45 +0200

    add countries data from csv data file

--- lib/oat_first.ex
+++ lib/oat_first.ex
@@ -6,4 +6,17 @@ defmodule OatFirst do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+
+  def get_countries() do
+    path = Application.app_dir(:oat_first, Path.join(["priv", "data", "countries.csv"]))
+    data = path |> File.stream!() |> CSV.decode!() |> Enum.to_list()
+    fields = hd(data)
+    records = tl(data)
+
+    records
+    |> Enum.map(fn values ->
+      Enum.zip_with(fields, values, fn k, v -> {k, v} end)
+      |> Map.new()
+    end)
+  end
 end
--- mix.exs
+++ mix.exs
@@ -56,7 +56,8 @@ defmodule OatFirst.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:csv, "~> 3.2.2"}
     ]
   end
 
