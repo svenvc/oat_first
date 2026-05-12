70ffdce358e963703fe2fa313c8898745f08e0ce
Sven Van Caekenberghe <sven@stfx.eu>
2026-05-12 10:27:20 +0200

    add a refresh button to system-info

--- lib/oat_first_web/live/system_info.ex
+++ lib/oat_first_web/live/system_info.ex
@@ -51,6 +51,23 @@ defmodule OatFirstWeb.Live.SystemInfo do
           </div>
 
           <div class="align-right mt-6">
+            <button phx-click="refresh" class="outline small">
+              <svg
+                width="16"
+                height="16"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="icon-light"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+                />
+              </svg>
+            </button>
             <.oat_theme_toggle />
           </div>
         </div>
@@ -65,6 +82,13 @@ defmodule OatFirstWeb.Live.SystemInfo do
     |> assign(:page_title, "System Info")
     |> assign(:system_info, get_system_info())
     |> then(&{:ok, &1})
+  end
+
+  @impl true
+  def handle_event("refresh", _unsigned_params, socket) do
+    socket
+    |> assign(:system_info, get_system_info())
+    |> then(&{:noreply, &1})
   end
 
   defp get_system_info() do
