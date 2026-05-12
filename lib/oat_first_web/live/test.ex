a392694c3f9958d2d68b87e06f53faadffbbfd26
Sven Van Caekenberghe <sven@beta9.be>
2026-05-12 17:01:37 +0200

    extend test with error flash and theme toggle

--- lib/oat_first_web/live/test.ex
+++ lib/oat_first_web/live/test.ex
@@ -4,8 +4,18 @@ defmodule OatFirstWeb.Live.Test do
   @impl true
   def render(assigns) do
     ~H"""
+    <Layouts.flash_group flash={@flash} />
+
     <h1>Test</h1>
-    <p><button phx-click="test-click">Go</button></p>
+
+    <div class="">
+      <button phx-click="test-click" class="outline small">Go</button>
+      <button phx-click="test-error" class="outline small">Error</button>
+    </div>
+
+    <div class="align-right mt-6">
+      <.oat_theme_toggle />
+    </div>
     """
   end
 
@@ -19,7 +29,14 @@ defmodule OatFirstWeb.Live.Test do
   @impl true
   def handle_event("test-click", _unsigned_params, socket) do
     socket
-    |> put_flash(:info, "You clicked the Go button")
+    |> put_flash(:info, "You clicked the Go button @ #{Time.utc_now() |> to_string()}")
+    |> then(&{:noreply, &1})
+  end
+
+  @impl true
+  def handle_event("test-error", _unsigned_params, socket) do
+    socket
+    |> put_flash(:error, "An error occurred @ #{Time.utc_now() |> to_string()}")
     |> then(&{:noreply, &1})
   end
 end
