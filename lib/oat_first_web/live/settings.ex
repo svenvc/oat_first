910b3b9d11cdd9f5ae14f916db9e5bfc6ae7facc
Sven Van Caekenberghe <sven@stfx.eu>
2026-05-13 15:42:11 +0200

    first version of dynamic boolean settings

--- lib/oat_first_web/live/settings.ex
+++ lib/oat_first_web/live/settings.ex
@@ -27,12 +27,42 @@ defmodule OatFirstWeb.Live.Settings do
       <div class="row">
         <div class="col-6 offset-3">
           <h1>Settings</h1>
+        </div>
+      </div>
 
+      <div
+        :for={
+          %{key: key, type: type, title: title, description: description} = _meta <- @settings_meta
+        }
+        class="row mt-4"
+      >
+        <div class="col-4 offset-3">
+          <div class="bold">{title}</div>
+          <div class="text-smaller text-light">{description}</div>
+        </div>
+        <div class="col-2 align-right">
+          <.settings_control name={key} type={type} value={@settings[key]} />
+        </div>
+      </div>
+
+      <br />
+
+      <div class="row mt-6">
+        <div class="col-6 offset-3">
+          <h1>Values</h1>
           <div class="table">
             <table>
+              <thead>
+                <tr>
+                  <th>key</th>
+                  <th>type</th>
+                  <th>value</th>
+                </tr>
+              </thead>
               <tbody>
-                <tr :for={%{key: key} <- @settings_meta}>
+                <tr :for={%{key: key, type: type} <- @settings_meta}>
                   <td>{key}</td>
+                  <td>{type}</td>
                   <td>{@settings[key]}</td>
                 </tr>
               </tbody>
@@ -48,6 +78,12 @@ defmodule OatFirstWeb.Live.Settings do
     """
   end
 
+  defp settings_control(%{type: :boolean} = assigns) do
+    ~H"""
+    <input type="checkbox" role="switch" name={@name} checked={@value} phx-click={"changed-boolean-#{@name}"} />
+    """
+  end
+
   @impl true
   def mount(_params, _session, socket) do
     socket
@@ -57,7 +93,16 @@ defmodule OatFirstWeb.Live.Settings do
     |> then(&{:ok, &1})
   end
 
-  def default_settings() do
+  @impl true
+  def handle_event("changed-boolean-" <> key, params, socket) do
+    socket
+    |> update(:settings, fn settings ->
+      settings |> Map.put(key, Map.get(params, "value") == "on")
+    end)
+    |> then(&{:noreply, &1})
+  end
+
+  defp default_settings() do
     @settings_meta |> Enum.into(%{}, fn %{key: key, default: default} -> {key, default} end)
   end
 end
